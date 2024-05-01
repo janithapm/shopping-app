bddd199a12e688f0c0f1e0849c3b801afb359e31
Janitha Madushanka <janitha.madushanka@circles.co>
2024-05-01 20:38:24 +0200

    changes to components

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../receipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+
+  recipes : Recipe[] = [
+    new Recipe("name1", "desc1", "https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=600"),
+    new Recipe("name2", "desc2", "https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=600"),
+  ];
 
 }
